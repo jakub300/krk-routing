4e288488ef9909f1c590ea71bf8c5aaa2e8c7af5
Jakub Bogucki <jakub300@gmail.com>
2019-02-08 23:10:08 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/recommended', '@vue/airbnb', 'plugin:prettier/recommended', 'prettier/vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/extensions': [
+      'error',
+      'always',
+      { js: 'never', mjs: 'never', jsx: 'never', ts: 'never', tsx: 'never', vue: 'never' },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
+  },
+  settings: {
+    'import/extensions': ['.vue'],
   },
 };
