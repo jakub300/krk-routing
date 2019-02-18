0dc0c183d3141401e144d7dbed61ab125bc1c9a0
Jakub Bogucki <jakub300@gmail.com>
2019-02-18 22:21:12 +0100

    Add script to download data and netlify build

---
+++ get-data.sh
@@ -0,0 +1,6 @@
+mkdir -p ./data/tram
+mkdir -p ./data/bus
+wget ftp://ztp.krakow.pl/GTFS_KRK_T.zip
+wget ftp://ztp.krakow.pl/GTFS_KRK_A.zip
+unzip -d ./data/tram ./GTFS_KRK_T.zip
+unzip -d ./data/bus ./GTFS_KRK_A.zip
