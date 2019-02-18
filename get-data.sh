e347fdad16c345f31c70052c0fe22f47f04f8834
Jakub Bogucki <jakub300@gmail.com>
2019-02-18 22:26:20 +0100

    Make downloads less noisy

--- get-data.sh
+++ get-data.sh
@@ -1,6 +1,6 @@
 mkdir -p ./data/tram
 mkdir -p ./data/bus
-wget ftp://ztp.krakow.pl/GTFS_KRK_T.zip
-wget ftp://ztp.krakow.pl/GTFS_KRK_A.zip
+wget -q ftp://ztp.krakow.pl/GTFS_KRK_T.zip
+wget -q ftp://ztp.krakow.pl/GTFS_KRK_A.zip
 unzip -d ./data/tram ./GTFS_KRK_T.zip
 unzip -d ./data/bus ./GTFS_KRK_A.zip
